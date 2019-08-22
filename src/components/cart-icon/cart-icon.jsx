261d2720f52e7373c725d45ad14a6817be7a2d8e
0zzycodes <horlalekan123@gmail.com>
2019-08-22 13:21:27 +0100

    Added cart-utils.js to group cart items in cart reduser and updated shop data

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -3,20 +3,22 @@ import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.scss';
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ cartItems, toggleCartHidden }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartItems.length}</span>
     </div>
   );
 };
-
+const mapStateToProps = state => ({
+  cartItems: state.cart.cartItems
+});
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
