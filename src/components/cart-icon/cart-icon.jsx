2cca890bae192020afaa4e24d300d39735fadaa0
0zzycodes <horlalekan123@gmail.com>
2019-08-22 17:59:15 +0100

    Added reselect to prevent some component from rerendering unnecessarily

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.scss';
-const CartIcon = ({ cartItems, toggleCartHidden }) => {
+const CartIcon = ({ itemCount, toggleCartHidden }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartItems.length}</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
 const mapStateToProps = state => ({
-  cartItems: state.cart.cartItems
+  itemCount: selectCartItemsCount(state)
 });
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
