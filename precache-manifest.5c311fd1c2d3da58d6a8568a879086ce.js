41a0d48c45e3817e5ac745c20f305061d2a51017
0zzycodes <horlalekan123@gmail.com>
2019-11-17 15:01:44 +0100

    Updates

---
+++ precache-manifest.5c311fd1c2d3da58d6a8568a879086ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a768a8c483f9e458bdc83579aa9c23c9",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "3b85d8b4ae859a618194",
+    "url": "/crwn-clothing/static/css/main.4c69c78b.chunk.css"
+  },
+  {
+    "revision": "a2798a503e7b7cd0401b",
+    "url": "/crwn-clothing/static/js/2.ac4229a9.chunk.js"
+  },
+  {
+    "revision": "3b85d8b4ae859a618194",
+    "url": "/crwn-clothing/static/js/main.de548021.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
