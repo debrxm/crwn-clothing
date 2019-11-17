5855908eb4ed02a54baa62b05b300084a6db91ba
0zzycodes <horlalekan123@gmail.com>
2019-11-17 15:12:45 +0100

    Ready to Deploy

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+import {
+  HashRouter as Router
+} from 'react-router-dom';
+import {
+  Provider
+} from 'react-redux';
+import {
+  PersistGate
+} from 'redux-persist/integration/react';
+import {
+  store,
+  persistor
+} from './redux/store';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+ReactDOM.render( <
+  Provider store = {
+    store
+  } >
+  <
+  Router >
+  <
+  PersistGate persistor = {
+    persistor
+  } >
+  <
+  App / >
+  <
+  /PersistGate> <
+  /Router> <
+  /Provider>,
   document.getElementById('root')
 );
